9d33a32b6a554ca5eecc4970f84a64ee860d12d2
Connor Hainje <cmhainje@gmail.com>
2021-07-02 17:38:58 -0400

    Overhaul structure and add many plots

--- main.js
+++ main.js
@@ -2,39 +2,38 @@
 const p_ranges = [
     '[0.5, 1.0] GeV', '[1.0, 1.5] GeV', '[1.5, 2.0] GeV', '[2.0, 2.5] GeV',
     '[2.5, 3.0] GeV', '[3.0, 3.5] GeV', '[3.5, 4.5] GeV'
-]
+];
 
 // Theta bin ranges
 const theta_ranges = [
     '[17°, 28°]', '[28°, 40°]',  '[40°, 60°]',   '[60°, 77°]',
     '[77°, 96°]', '[96°, 115°]', '[115°, 133°]', '[133°, 150°]',
-]
+];
 
 // References to the relevant elements
 const image = document.getElementById('image');
 const image_selector = document.getElementById('image-select');
+const dset_selector = document.getElementById('dset-select');
 const bin_selector = document.getElementById('bin-select');
 const slider_p = document.getElementById('slider-p');
 const slider_theta = document.getElementById('slider-theta');
 const string_p = document.getElementById('string-p');
 const string_theta = document.getElementById('string-theta');
-const warning_text = document.getElementById('warning');
 
 // Event listeners
 function makeFilename() {
-    if (bin_selector.value == "nobin") {
-        return image_selector.value + ".png";
-    }
-    else {
-        return image_selector.value
-            + "/p" + slider_p.value 
-            + "_theta" + slider_theta.value 
-            + ".png";
-    }
+    let s = dset_selector.value + "/" + image_selector.value;
+    if (bin_selector.value == "bin")
+        s += "p" + slider_p.value + "_theta" + slider_theta.value;
+    s += ".png";
+    return s;
 }
 
+dset_selector.addEventListener('change', (event) => {
+    image.src = makeFilename();
+})
+
 image_selector.addEventListener('change', (event) => {
-    const folder_end_index = image.src.lastIndexOf('/p');
     image.src = makeFilename();
 })
 
@@ -44,7 +43,8 @@ bin_selector.addEventListener('change', (event) => {
         slider_p.style.display = 'inline';
         string_theta.style.display = 'inline';
         slider_theta.style.display = 'inline';
-        warning_text.style.display = 'none';
+        for (el of document.getElementsByClassName('nobinonly'))
+            el.disabled = true;
         image.src = makeFilename();
     }
 
@@ -53,7 +53,8 @@ bin_selector.addEventListener('change', (event) => {
         slider_p.style.display = 'none';
         string_theta.style.display = 'none';
         slider_theta.style.display = 'none';
-        warning_text.style.display = 'inline';
+        for (el of document.getElementsByClassName('nobinonly'))
+            el.disabled = false;
         image.src = makeFilename();
     }
 })
