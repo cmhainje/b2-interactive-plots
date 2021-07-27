5d82c756186a16853982ba31b5435fba5e231445
Connor Hainje <cmhainje@gmail.com>
2021-07-27 11:49:13 -0400

    Bugfix: fracSupported no longer returns true for new plot types

--- main.js
+++ main.js
@@ -129,7 +129,7 @@ function deltaSupported() {
 }
 
 function fracSupported() {
-    if (type_select.value == "accu" || type_select.value == "ctrb")
+    if (type_select.value != "conf")
         return false;
     if (deltaSupported() && dlt_check_is_on)
         return false;
